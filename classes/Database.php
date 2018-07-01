f39d28e963b1fd906194d951f7d64d7a09bdb50f
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-01 19:02:46 +0200

    add function connection to class Database

--- classes/Database.php
+++ classes/Database.php
@@ -1,7 +1,14 @@
 <?php
 
 class Database {
-    
+    // connection
+    public function connection(){
+        try {
+            $conn = new PDO('mysql:host=localhost; dbname=test', 'root', ''); 
+        } catch (PDOException $e){
+            print_r($e->getMessage); 
+        }
+    }
 }
 
 ?>
