b69aa8fef438c6e84cda92ff1b7e71c742266113
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-03 17:43:42 +0200

    Database.php: add select function

--- classes/Database.php
+++ classes/Database.php
@@ -4,11 +4,36 @@ class Database {
     // connection
     public function connection(){
         try {
-            $conn = new PDO('mysql:host=localhost; dbname=test', 'root', ''); 
+            $conn = new PDO('mysql:host=localhost; dbname=todo', 'root', ''); 
         } catch (PDOException $e){
             print_r($e->getMessage); 
         }
         echo "connection ok"; 
+
+        return $conn; 
+
+        // check if user exists 
+
+        /*$username = "Caroline"; 
+        $query = $conn->prepare("SELECT * FROM user WHERE username = :username");
+        $query->bindParam(":username", $username); 
+        $res = $query->execute();
+        while ($res = $query->fetch(PDO::FETCH_ASSOC)){
+            print_r($res); 
+        }*/
+    }
+
+    // select query
+    public function select($name, $value){
+        $query = $conn->prepare("SELECT * FROM user WHERE :name = :value");
+        $query->bindParam(":name", $name); 
+        $query->bindParam(":value", $value); 
+        $result = $query->execute();
+
+        // show in array
+        while ($res = $query->fetch(PDO::FETCH_ASSOC)){
+            print_r($res); 
+        }
     }
 }
 
--- classes/User.php
+++ classes/User.php
@@ -8,14 +8,14 @@ class User {
     }
 
     function checklogin(){
-        
+
     }
     // check if user exists 
-    $query = $db->prepare("SELECT * FROM user WHERE username = :username");
+    /*$query = $db->prepare("SELECT * FROM user WHERE username = :username");
     $query->bindParam(":username", $this->$username); 
     $res = $query->execute();
     $user = $stm->fetch(PDO::FETCH_ASSOC);
-    echo $user; 
+    echo $user; */
 }
 
 ?>
