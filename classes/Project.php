d5e50232031317c8bc3132cb6a03c215fbf6bcf7
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-08 16:08:29 +0200

    add private variables to class Project

---
+++ classes/Project.php
@@ -0,0 +1,17 @@
+<?php
+
+class Project extends Database {
+    /** variables */
+    private $title;
+    private $studentId;
+    private $projectId; 
+    private $adminId; 
+    private $tasks;
+    private $people; 
+
+    /** getters */
+
+    /** setters */
+}
+
+?>
