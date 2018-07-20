1cf8562a7d3f554461c87b6357226a8904d7829b
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-20 11:47:23 +0200

    create new folder uploads

---
+++ upload.php
@@ -0,0 +1,17 @@
+<?php
+ // source: http://php.net/manual/en/features.file-upload.php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit" >upload image</button>
+
+</form> 
+
+</body>
+</html>
