71b0d2b79811133eb8fe0e1f5f9ac2eeb66195d3
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-22 15:05:42 +0200

    register.php: upload picture layout

--- register.php
+++ register.php
@@ -76,7 +76,7 @@ if (!empty($_POST)){
 
 <body>
     <div class="login">
-        <form method="post">
+        <form method="post" enctype="multipart/formdata">
             <h2 class="sr-only">Register Form</h2>
             <div class="illustration">
                 <i class="fas fa-file-alt"></i>
@@ -100,14 +100,15 @@ if (!empty($_POST)){
             <div class="form-group">
                 <input class="form-control" type="password" name="password2" placeholder="Repeat Password">
             </div>
-            <!-- upload picture 
+            
+            <!-- upload picture --> 
 
             <div class="form-group">
-                <input class="form-control" type="file" name="img">
-            </div>-->
+                <input class="form-control" type="file" name="img" id="img">
+            </div>
 
             <div class="form-group">
-                <button class="btn btn-primary btn-block" type="submit">Create Account</button>
+                <button class="btn btn-primary btn-block" type="submit" name="submit">Create Account</button>
             </div>
             <a href="login.php" class="loginlink">Login</a>
         </form>
