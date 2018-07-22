1f53c5e2d5018d19433cbd736b22fcb63d4341d2
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-22 15:39:21 +0200

    check if uploaded file is smaller then 1mb

--- uploadPicture.php
+++ uploadPicture.php
@@ -2,7 +2,7 @@
 if (isset($_POST['submit'])){
     echo "ok"; 
     $img = $_FILES['uploadimg'];
-    print_r($img); 
+    //print_r($img); 
 
     //name
     $imgName = $_FILES['uploadimg']['name'];
@@ -16,7 +16,13 @@ if (isset($_POST['submit'])){
 
     // check if file is img
     if (in_array($imgExt, $images)){
-        echo "ok img"; 
+        //echo "ok img"; 
+
+        // check img size - max 1mb 
+        if ($_FILES['uploadimg']['size'] < 100000){
+            echo "size ok"; 
+
+        }
     } else {
         echo "file is not an image"; 
     }
