f3bb53fb547397b1e1138d005f8b0f9f8124f70c
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-22 15:28:37 +0200

    get name and extension of uploaded file

--- uploadPicture.php
+++ uploadPicture.php
@@ -1,7 +1,18 @@
 <?php
 if (isset($_POST['submit'])){
     echo "ok"; 
-    $file = $_FILES['uploadimg'];
+    $img = $_FILES['uploadimg'];
     //print_r($file); 
+
+    //name
+    $imgName = $_FILES['img']['name'];
+
+    // get extension after . 
+    $imgExtension = explode('.', $imgName); 
+    $imgExt = strtolower(end($imgExtension));
+
+    // only allow images 
+    $images = array('jpg', 'jpeg', 'png');
+
 }
 ?>
