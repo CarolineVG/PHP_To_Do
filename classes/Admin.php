ebe54427302478467bfc86588359adc1cdd27c30
Caroline VG <CarolineVG@users.noreply.github.com>
2018-07-27 18:52:49 +0200

    create class Admin

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,7 +1,59 @@
 <?php
 
-class Admin {
-    
+class Admin extends Database {
+    /** variables */
+    private $adminId; 
+    private $name; 
+    private $pass;
+    private $education;
+    private $email; 
+
+    /** getters */
+    public function getAdminId(){
+        return $this->adminId; 
+    }
+
+    public function getName(){
+        return $this->name; 
+    }
+
+    public function getPass(){
+        return $this->pass; 
+    }
+
+    public function getEducation(){
+        return $this->education; 
+    }
+
+    public function getEmail(){
+        return $this->email; 
+    }
+
+    /** setters */
+    public function setAdminId($adminId){
+        $this->adminId = $adminId;
+        return $this; 
+    }
+
+    public function setName($name){
+        $this->name = $name;
+        return $this; 
+    }
+
+    public function setPass($pass){
+        $this->pass = $pass;
+        return $this; 
+    }
+
+    public function setEducation($education){
+        $this->education = $education;
+        return $this; 
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+        return $this; 
+    }
 }
 
 ?>
