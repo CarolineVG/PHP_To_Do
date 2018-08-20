a3f7f0c65bc48e7331c6d9c088db0e90674c9a2d
Caroline VG <CarolineVG@users.noreply.github.com>
2018-08-20 18:36:54 +0200

    test layout of work bar chart

---
+++ deadlines.php
@@ -0,0 +1,21 @@
+
+<?php
+/** header */
+include_once("header.php");
+?>
+
+    <div class="chart">
+        <div class="bar monday">Monday</div>
+        <div class="bar thuesday">Thuesday</div>
+        <div class="bar wednesday">Wednesday</div>
+        <div class="bar thursday">Thursday</div>
+        <div class="bar friday">Friday</div>
+        <div class="bar saturday">Saturday</div>
+        <div class="bar sunday">Sunday</div>
+    </div>
+
+
+<?php
+/** header */
+include_once("header.php");
+?>
